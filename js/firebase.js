57a8b676a3a27a9fe27a74ca245f93500899553f
alai-ali <alaistroy07@gmail.com>
2026-03-11 00:09:54 +0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -68,7 +68,26 @@ function loadFirebase() {
   s1.src = 'https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js';
   s1.onload = function() {
     var s2 = document.createElement('script');
-    s2.src = 'https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js';
+   s2.src='https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js';
+s2.onload=function(){
+  var s3=document.createElement('script');
+  s3.src='https://www.gstatic.com/firebasejs/9.23.0/firebase-database-compat.js';
+  s3.onload=function(){
+    if(!firebase.apps.length){
+      firebase.initializeApp({
+        apiKey:'AIzaSyBlD8lNdYdubHXr13IhPkmkCnNQQLChtVA',
+        authDomain:'bsmlh-chat.firebaseapp.com',
+        databaseURL:'https://bsmlh-chat-default-rtdb.firebaseio.com',
+        projectId:'bsmlh-chat',
+        storageBucket:'bsmlh-chat.firebasestorage.app',
+        messagingSenderId:'41774666354',
+        appId:'1:41774666354:web:e200d57a0bab89e26be8eb'
+      });
+    }
+    setTimeout(initJobsDB, 500);
+  };
+  document.head.appendChild(s3);
+};
     s2.onload = function() {
       if (!firebase.apps.length) {
         firebase.initializeApp({
